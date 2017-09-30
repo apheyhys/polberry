514d934a6be3ff499a1fe6b2da260d65d2eee957
apheyhys <apheyhys@gmail.com>
2017-09-30 23:34:39 +0300

    pep8

--- person.py
+++ person.py
@@ -1,6 +1,6 @@
 person3 = {'Name':'Ford Prefect',
- 'Gender': 'Male',
- 'Occupation': 'Researcher',
- 'Home Planet': 'Betelgeuse Seven'}
+           'Gender': 'Male',
+           'Occupation': 'Researcher',
+           'Home Planet': 'Betelgeuse Seven'}
 person3['Age'] = 33
 print(person3)
--- vsearch.py
+++ vsearch.py
@@ -1,3 +1,3 @@
-def search4letters(phrase:str, letters:str) -> set:
+def search4letters(phrase: str, letters: str) -> set:
     """Возвращает множество букв из 'letters', найденных в указанной фразе."""
     return set(letters).intersection(set(phrase))
